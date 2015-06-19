b1e4e2fb8a8c83a4cf3f4ce4b5eecb136f9cd801
idkkk <idkkk@163.com>
2015-06-19 17:08:07 +0800

    删除无用的类

--- showcase-utils/src/main/java/org/rubik/sandobx/utils/WordFilter.java
+++
@@ -1,8 +0,0 @@
-package org.rubik.sandobx.utils;
-
-public interface WordFilter {
-	public void insertToTree(String words);
-	public void insertToTree(String[] words);
-	public String eliminateMatchingWords(String text);
-	public int getMatchCount(String text);
-}
